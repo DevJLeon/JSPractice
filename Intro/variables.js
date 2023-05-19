0286346c5b42d4947ca76487b68dd6807554c5ce
Joshua <celisjoshleon@gmail.com>
2023-05-19 17:10:58 -0500

    first commit

---
+++ Intro/variables.js
@@ -0,0 +1,19 @@
+var edad = 21; //Public (access from anywhere in the code)
+let nombre = "Joshua"; //private (Accesable from specific parts of the code)
+const eMail = "correo@correo.com" //private - Cannot be modified
+
+let telefono, pais, id;
+
+telefono  = 3005867777
+pais = "Ughanda"
+id = 100555555
+
+const resultado = 4+4; // se almacena el resultado de la suma
+
+//Concatenar:
+const nom1 = "Carlos";
+const nom2 = "Arturo";
+
+const nomCompleto = nom1+nom2;
+console.log(nomCompleto); //:CarlosArturo
+
