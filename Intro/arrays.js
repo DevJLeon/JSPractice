5a42da201a0559085ea12f5965d91394d4a15eec
DevJLeon <celisjoshleon@gmail.com>
2023-05-20 01:35:42 -0500

    arrays.js created

---
+++ Intro/arrays.js
@@ -0,0 +1,7 @@
+const arrayPrueba = ["text", 456.1, false, {nombre:"Juanito"}[3,1,5,2]];
+
+const array = ["mango", "pera"];
+//METHODS
+array.push("manzana"); // Equivalent to insert in PY
+
+console.log(array);
