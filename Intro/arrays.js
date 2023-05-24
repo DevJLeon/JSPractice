b42793a1e9aa49e1d2d89d006a688c64807eaaa9
DevJLeon <celisjoshleon@gmail.com>
2023-05-23 20:49:27 -0500

    Arrays

--- Intro/arrays.js
+++ Intro/arrays.js
@@ -3,5 +3,21 @@ const arrayPrueba = ["text", 456.1, false, {nombre:"Juanito"}[3,1,5,2]];
 const array = ["mango", "pera"];
 //METHODS
 array.push("manzana"); // Equivalent to insert in PY
+array.pop(); // Remove the last element
 
 console.log(array);
+console.log(array.length);
+console.log(array[2]);
+
+const a = Array.of("X", "Y", "Z", 1, 2, 3);
+console.log(a);
+
+const b = Array(10).fill("Hola");
+console.log(b);
+
+const colors = ["Red", "Green", "Blue"];
+
+colors.forEach(function(el,index){
+    console.log(`<li id="${index}">${el}</li>`)
+})
+
